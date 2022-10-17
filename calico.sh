d9d8fbcfcf1520612cc1ba45bcadaf8ba891086f
Nigel Douglas <109959738+n1g3ld0ugla5@users.noreply.github.com>
2022-10-17 21:38:01 +0100

    Update calico.sh

--- calico.sh
+++ calico.sh
@@ -7,16 +7,31 @@ sed -i 's/#   value: "192.168.0.0\/16"/  value: "192.168.0.0\/20"/g' calico.yaml
 cat calico.yaml | grep CALICO_IPV4POOL_CIDR -A1
 
 kubectl apply -f ./calico.yaml
+kubectl create ns cis-benchmarks
 
 # Install Kube-Bench
-wget https://raw.githubusercontent.com/aquasecurity/kube-bench/main/job.yaml
+wget https://raw.githubusercontent.com/aquasecurity/kube-bench/main/job.yaml -n cis-benchmarks
 kubectl apply -f job.yaml
 kubectl logs -l "app=kube-bench"
 
 # Kube-Bench Job Node
-wget https://raw.githubusercontent.com/aquasecurity/kube-bench/main/job-node.yaml
+wget https://raw.githubusercontent.com/aquasecurity/kube-bench/main/job-node.yaml -n cis-benchmarks
 kubectl apply -f job.yaml
 
+# Kube-Bench Job Master
+wget https://raw.githubusercontent.com/aquasecurity/kube-bench/main/job-master.yaml -n cis-benchmarks
+kubectl apply -f job-master.yaml
+
+# Waits 10 seconds before checking comopliance reports
+sleep 10s
+
 # Health Checks
-kubectl logs -l "app=kube-bench"
-kubectl logs -l "job-name=kube-bench"
+kubectl logs -l "app=kube-bench"  -n cis-benchmarks
+kubectl logs -l "job-name=kube-bench"  -n cis-benchmarks
+kubectl logs -l "job-name=kube-bench-master"  -n cis-benchmarks
+
+# Confirm Cluster Configuration
+kubectl version
+kubectl cluster-info
+sudo docker run --rm -v 'pwd':/host aquasec/kube-bench:latest install
+./kube-bench
