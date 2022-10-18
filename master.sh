16d55c7e2a2238bbb81c401dbf70843437fa827e
Nigel Douglas <109959738+n1g3ld0ugla5@users.noreply.github.com>
2022-10-18 10:45:26 +0100

    Update master.sh

--- master.sh
+++ master.sh
@@ -53,3 +53,6 @@ mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 kubectl taint nodes --all node-role.kubernetes.io/master-
+
+# Confirm one master node is created
+kubectl get nodes -o wide
